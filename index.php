c84e464e9b3a99f717967839e833cfa34463407d
Sun05 <63901549+Sun05@users.noreply.github.com>
2020-04-30 18:33:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php 
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script data-ad-client="ca-pub-2962676064831038" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DiscordApe</title>
+</head>
+<body>
+    <h1>
+        This is my website.
+    </h1>
+    <h2>
+        It is called DiscordApe.
+    </h2>
+    <h2>It is about Discord and Apes.</h2>
+    <p>Here are some Discord and Apes</p>
+    <img src="https://images.pexels.com/photos/39571/gorilla-silverback-animal-silvery-grey-39571.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
+    <img src="https://images.pexels.com/photos/41303/animal-ape-black-clever-41303.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
+    <img src="https://images.pexels.com/photos/321552/pexels-photo-321552.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
+    <p>Thank you to pexels.com for image. Check back for more image when site updated.</p>
+    <h2>Thats apes, heres the discord: discord.gg/8ranDV7</h2>
+</body>
+</html>
+'
+  ?>
